daf99e7e71e3666a0fcfba25810cec2c397b18ff
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-10-27 20:39:30 +0400

    Add files via upload

---
+++ Rectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RectangleHeight rectangle = new RectangleHeight(5, 10);
+            double area = rectangle.CalculateArea();
+            Console.WriteLine("{area}");
+        }
+    }
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Rectangle
+{
+    protected double side1;
+    protected double side2;
+
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+}
+
+class RectangleHeight : Rectangle
+{
+    private double height;
+
+    public RectangleHeight(double side1, double side2) : base(side1, side2)
+    {
+        this.height = side2;
+    }
+
+    public double CalculateArea()
+    {
+        double s = side1 * side2;
+        return s;
+    }
+}
