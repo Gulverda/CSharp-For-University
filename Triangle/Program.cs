2342fa0b397f59babc509f5876a1e2d9c1db3e21
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-10-27 20:16:41 +0400

    Add files via upload

---
+++ Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleWithPerimeter triangle = new TriangleWithPerimeter(5, 12, 13);
+            double area = triangle.CalculateArea();
+            double perimeter = triangle.CalculatePerimeter();
+            Console.WriteLine($"Area: {area}, Perimeter: {perimeter}");
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class Triangle
+{
+    protected double side1;
+    protected double side2;
+    protected double side3;
+
+    public Triangle(double side1, double side2, double side3)
+    {
+        this.side1 = side1; 
+        this.side2 = side2; 
+        this.side3 = side3;
+    }
+}
+
+class TriangleWithPerimeter : Triangle
+{
+    private double perimeter;
+    public TriangleWithPerimeter(double side1, double side2, double side3) : base(side1, side2, side3)
+    {
+        perimeter = side1 + side2 + side3;
+    }
+
+    public double CalculateArea()
+    {
+        double s = perimeter / 2;
+        double area = Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        return area;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return perimeter;
+    }
+}
+
