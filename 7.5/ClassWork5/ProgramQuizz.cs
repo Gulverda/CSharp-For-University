0d6ab6569d13eb343fb65e601c1434d2fdc31b1c
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-11-02 19:17:33 +0400

    Add files via upload

---
+++ 7.5/ClassWork5/ProgramQuizz.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> clients = new List<Client>
+        {
+            new Depositor
+            {
+                Name = "John",
+                Surname = "Doe",
+                PersonalNumber = "123456789",
+                DateOfOpeningDeposit = new DateTime(2023, 1, 15),
+                AmountOfMoney = 50000,
+                InterestRate = 2.5
+            },
+            new Creditor
+            {
+                Name = "Alice",
+                Surname = "Smith",
+                PersonalNumber = "987654321",
+                DateOfIssuanceOfLoan = new DateTime(2023, 3, 10),
+                LoanAmount = 100000,
+                InterestRate = 5.0,
+                RemainingAmount = 75000
+            },
+            new Organization
+            {
+                Name = "ABC Inc.",
+                IdentificationCode = "789012345",
+                DateOfOpeningAccount = new DateTime(2023, 2, 5),
+                AccountTitle = "Business Account",
+                AccountAmount = 200000
+            }
+        };
+
+            // Show information about all clients
+            Console.WriteLine("Information about all clients:");
+            foreach (var client in clients)
+            {
+                client.ShowInfo();
+                Console.WriteLine();
+            }
+
+            // Sort clients by decreasing debt (for creditors)
+            var creditors = clients.OfType<Creditor>().OrderByDescending(c => c.RemainingAmount).ToList();
+
+            Console.WriteLine("Information about creditors sorted by decreasing debt:");
+            foreach (var creditor in creditors)
+            {
+                creditor.ShowInfo();
+                Console.WriteLine();
+            }
+
+            // Search mechanism by date interval
+            DateTime startDate = new DateTime(2023, 1, 1);
+            DateTime endDate = new DateTime(2023, 2, 28);
+
+            Console.WriteLine("Information about clients who started cooperating within the specified date interval:");
+            foreach (var client in clients)
+            {
+                if (client.IsInDateInterval(startDate, endDate))
+                {
+                    client.ShowInfo();
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ 7.5/ClassWork5/Quizz.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Client
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PersonalNumber { get; set; }
+
+    public abstract void ShowInfo();
+    public abstract bool IsInDateInterval(DateTime startDate, DateTime endDate);
+}
+
+public class Depositor : Client
+{
+    public DateTime DateOfOpeningDeposit { get; set; }
+    public decimal AmountOfMoney { get; set; }
+    public double InterestRate { get; set; }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Depositor: {Name} {Surname}");
+        Console.WriteLine($"Personal Number: {PersonalNumber}");
+        Console.WriteLine($"Date of Opening Deposit: {DateOfOpeningDeposit.ToShortDateString()}");
+        Console.WriteLine($"Amount of Money: {AmountOfMoney:C}");
+        Console.WriteLine($"Interest Rate: {InterestRate}%");
+    }
+
+    public override bool IsInDateInterval(DateTime startDate, DateTime endDate)
+    {
+        return DateOfOpeningDeposit >= startDate && DateOfOpeningDeposit <= endDate;
+    }
+}
+
+public class Creditor : Client
+{
+    public DateTime DateOfIssuanceOfLoan { get; set; }
+    public decimal LoanAmount { get; set; }
+    public double InterestRate { get; set; }
+    public decimal RemainingAmount { get; set; }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Creditor: {Name} {Surname}");
+        Console.WriteLine($"Personal Number: {PersonalNumber}");
+        Console.WriteLine($"Date of Issuance of Loan: {DateOfIssuanceOfLoan.ToShortDateString()}");
+        Console.WriteLine($"Loan Amount: {LoanAmount:C}");
+        Console.WriteLine($"Interest Rate: {InterestRate}%");
+        Console.WriteLine($"Remaining Amount: {RemainingAmount:C}");
+    }
+
+    public override bool IsInDateInterval(DateTime startDate, DateTime endDate)
+    {
+        return DateOfIssuanceOfLoan >= startDate && DateOfIssuanceOfLoan <= endDate;
+    }
+}
+
+public class Organization : Client
+{
+    public string IdentificationCode { get; set; }
+    public DateTime DateOfOpeningAccount { get; set; }
+    public string AccountTitle { get; set; }
+    public decimal AccountAmount { get; set; }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Organization: {Name}");
+        Console.WriteLine($"Identification Code: {IdentificationCode}");
+        Console.WriteLine($"Date of Opening Account: {DateOfOpeningAccount.ToShortDateString()}");
+        Console.WriteLine($"Account Title: {AccountTitle}");
+        Console.WriteLine($"Account Amount: {AccountAmount:C}");
+    }
+
+    public override bool IsInDateInterval(DateTime startDate, DateTime endDate)
+    {
+        return DateOfOpeningAccount >= startDate && DateOfOpeningAccount <= endDate;
+    }
+}
