1e0f73e224995b1568dce950c04051488ce36c2f
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-11-15 19:56:39 +0400

    Add files via upload

---
+++ 7.5/ClassWork5/Quizz2/ProgramQuizz2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qvizi
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a list of publications instead of an array
+            List<Publication> publications = new List<Publication>
+        {
+            new Book
+            {
+                Title = "Sample Book",
+                Authors = new string[] { "John Doe" },
+                YearOfPublication = 2020,
+                Publisher = "Publisher A",
+                NumberOfPages = 200
+            },
+            new Article
+            {
+                Title = "Sample Article",
+                Authors = new string[] { "Jane Smith" },
+                YearOfPublication = 2021,
+                JournalName = "Journal X",
+                VolumeNumber = 3,
+                PagesQuantity = 15
+            },
+            new ElectronicResource
+            {
+                Title = "Sample Electronic Resource",
+                Authors = new string[] { "Sam Johnson" },
+                Link = "https://example.com",
+                Annotation = "This is a sample electronic resource."
+            }
+            // Add more publications as needed
+        };
+
+            // Display information about all editions (sorted by title)
+            var sortedPublications = publications.OrderBy(p => p.Title);
+            foreach (var publication in sortedPublications)
+            {
+                publication.DisplayDetailedInfo();
+                Console.WriteLine("------------------------------");
+            }
+
+            // Activate the search engine to display information within a date range
+            int startYear = 2019;
+            int endYear = 2022;
+            Console.WriteLine($"Publications within the date range {startYear}-{endYear}:");
+            var publicationsInRange = publications.Where(p => p.IsInDateRange(startYear, endYear));
+            foreach (var publication in publicationsInRange)
+            {
+                publication.DisplayDetailedInfo();
+                Console.WriteLine("------------------------------");
+            }
+        }
+    }
+
+
+}
---
+++ 7.5/ClassWork5/Quizz2/quizz2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Abstract class Publication
+public abstract class Publication
+{
+    public string Title { get; set; }
+    public string[] Authors { get; set; }
+    public int YearOfPublication { get; set; }
+
+    // DisplayDetailedInfo is an abstract method that must be implemented by the derived classes.
+    public abstract void DisplayDetailedInfo();
+
+    // IsInDateRange is a method that checks if the publication is within a given date range.
+    public abstract bool IsInDateRange(int startYear, int endYear);
+}
+
+// Book class (inherits from Publication)
+public class Book : Publication
+{
+    public string Publisher { get; set; }
+    public int NumberOfPages { get; set; }
+
+    // DisplayDetailedInfo method provides information specific to a book.
+    public override void DisplayDetailedInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Authors: {string.Join(", ", Authors)}");
+        Console.WriteLine($"Year of Publication: {YearOfPublication}");
+        Console.WriteLine($"Publisher: {Publisher}");
+        Console.WriteLine($"Number of Pages: {NumberOfPages}");
+    }
+
+    // IsInDateRange method checks if the book is within a given date range.
+    public override bool IsInDateRange(int startYear, int endYear)
+    {
+        return YearOfPublication >= startYear && YearOfPublication <= endYear;
+    }
+}
+
+// Article class (inherits from Publication)
+public class Article : Publication
+{
+    public string JournalName { get; set; }
+    public int VolumeNumber { get; set; }
+    public int PagesQuantity { get; set; }
+
+    // DisplayDetailedInfo method provides information specific to an article.
+    public override void DisplayDetailedInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Authors: {string.Join(", ", Authors)}");
+        Console.WriteLine($"Journal Name: {JournalName}");
+        Console.WriteLine($"Volume Number: {VolumeNumber}");
+        Console.WriteLine($"Year of Publication: {YearOfPublication}");
+        Console.WriteLine($"Pages Quantity: {PagesQuantity}");
+    }
+
+    // IsInDateRange method checks if the article is within a given date range.
+    public override bool IsInDateRange(int startYear, int endYear)
+    {
+        return YearOfPublication >= startYear && YearOfPublication <= endYear;
+    }
+}
+
+// ElectronicResource class (inherits from Publication)
+public class ElectronicResource : Publication
+{
+    public string Link { get; set; }
+    public string Annotation { get; set; }
+
+    // DisplayDetailedInfo method provides information specific to an electronic resource.
+    public override void DisplayDetailedInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Authors: {string.Join(", ", Authors)}");
+        Console.WriteLine($"Link: {Link}");
+        Console.WriteLine($"Annotation: {Annotation}");
+    }
+
+    // IsInDateRange method for electronic resources may not have a specific year of publication.
+    // You can modify this method as per your requirements.
+    public override bool IsInDateRange(int startYear, int endYear)
+    {
+        // Electronic resources may not have a specific year of publication
+        // You can modify this method as per your requirements
+        return true;
+    }
+}
+
