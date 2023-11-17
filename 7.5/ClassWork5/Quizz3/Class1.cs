e37273d2d513bce889e97ac6018fcf696b833c83
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-11-17 07:50:49 -0800

    Add files via upload

---
+++ 7.5/ClassWork5/Quizz3/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+public abstract class Software
+{
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public DateTime ReleaseDate { get; set; }
+
+    public abstract void DisplayInfo();
+
+    public abstract bool IsSuitable();
+}
+
+public class Free : Software
+{
+    public Free(string name, string manufacturer, DateTime releaseDate)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        ReleaseDate = releaseDate;
+    }
+
+    public override void DisplayInfo()
+    {
+        //throw new NotImplementedException();
+        Console.WriteLine($"Free Software: {Name},\n Manufacturer: {Manufacturer},\n Release Date: {ReleaseDate.ToShortDateString()}");
+    }
+
+    public override bool IsSuitable()
+    {
+        return true;
+    }
+}
+
+public class Trial : Software
+{
+    public DateTime InstallDate { get; set; }
+    public int FreePeriodMonth { get; set; }
+
+    public Trial(string name, string manufacturer, DateTime releaseDate, DateTime installDate, int freePeriodMonths)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        ReleaseDate = releaseDate;
+        InstallDate = installDate;
+        FreePeriodMonth = freePeriodMonths;
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Trial Software: {Name},\n Manufacturer: {Manufacturer},\n Release Date: {ReleaseDate.ToShortDateString()},\n Install Date: {InstallDate.ToShortDateString()},\n Free Period: {FreePeriodMonth} months");
+
+    }
+
+    public override bool IsSuitable()
+    {
+        return DateTime.Now < InstallDate.AddMonths(FreePeriodMonth);
+
+    }
+}
+
+public class Commercial : Software
+{
+    public decimal Price { get; set; }
+    public DateTime InstallDate { get; set; }
+    public int TermOfUseMonths { get; set; }
+
+    public Commercial(string name, string manufacturer, DateTime releaseDate, decimal price, DateTime installDate, int termOfUseMonths)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        ReleaseDate = releaseDate;
+        Price = price;
+        InstallDate = installDate;
+        TermOfUseMonths = termOfUseMonths;
+    }
+
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Commercial Software: {Name},\n Manufacturer: {Manufacturer}, \n Release Date: {ReleaseDate.ToShortDateString()},\n Price: {Price:C},\n Install Date: {InstallDate.ToShortDateString()},\n Term of Use: {TermOfUseMonths} months");
+
+    }
+
+    public override bool IsSuitable()
+    {
+        return DateTime.Now < InstallDate.AddMonths(TermOfUseMonths);
+    }
+}
---
+++ 7.5/ClassWork5/Quizz3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Software[] softwareArray = new Software[]
+            {
+            new Free("FreeSoftware1", "ManufacturerA", DateTime.Parse("2022-01-01")),
+            new Trial("TrialSoftware1", "ManufacturerB", DateTime.Parse("2022-03-01"), DateTime.Parse("2022-08-01"), 3),
+            new Commercial("CommercialSoftware1", "ManufacturerC", DateTime.Parse("2022-03-01"), 99.99m, DateTime.Parse("2024-04-01"), 6),
+            };
+
+            Console.WriteLine("All Software Information (Ascending order by days until expiration):");
+            var sortedSoftware = softwareArray.OrderBy(s => s.IsSuitable()).ThenBy(s => s.ReleaseDate);
+            foreach (var software in sortedSoftware)
+            {
+                software.DisplayInfo();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Not Expired Software Information:");
+            var notExpiredSoftware = softwareArray.Where(s => s.IsSuitable());
+            foreach (var software in notExpiredSoftware)
+            {
+                software.DisplayInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
